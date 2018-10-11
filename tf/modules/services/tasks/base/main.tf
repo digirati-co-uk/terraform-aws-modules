c8b1490c5d18e7eda00523a5a6f05c9e41e3fc16
Adam Christie <fractos@gmail.com>
2018-10-11 14:58:02 +0100

    fix

--- tf/modules/services/tasks/base/main.tf
+++ tf/modules/services/tasks/base/main.tf
@@ -2,14 +2,14 @@ data "template_file" "definition" {
   template = "${file("${path.module}/files/task_definition.jsontemplate")}"
 
   vars {
-    prefix           = "${var.prefix}"
-    log_group_name   = "${var.log_group_name}"
-    log_group_region = "${var.log_group_region}"
-    log_prefix       = "${var.log_prefix}"
-    environment_vars = "${module.env_vars.env_vars_string}"
-    container_name   = "${var.container_name}"
-    container_port   = "${var.container_port}"
-    docker_image     = "${var.docker_image}"
+    prefix                = "${var.prefix}"
+    log_group_name        = "${var.log_group_name}"
+    log_group_region      = "${var.log_group_region}"
+    log_prefix            = "${var.log_prefix}"
+    environment_variables = "${module.env_vars.env_vars_string}"
+    container_name        = "${var.container_name}"
+    container_port        = "${var.container_port}"
+    docker_image          = "${var.docker_image}"
   }
 }
 
