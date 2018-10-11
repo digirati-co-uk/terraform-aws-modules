a4e880787a286f84fd727ccd2d9749e645c108bb
Adam Christie <fractos@gmail.com>
2018-10-11 11:53:27 +0100

    fix arn

--- tf/modules/iam-baseline/iam-group-policy-attachments.tf
+++ tf/modules/iam-baseline/iam-group-policy-attachments.tf
@@ -5,11 +5,7 @@ resource "aws_iam_group_policy_attachment" "force_mfa_admins" {
   policy_arn = "${aws_iam_policy.force_mfa.arn}"
 }
 
-data "aws_iam_policy" "administrator_access" {
-  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
 resource "aws_iam_group_policy_attachment" "administrator_access" {
   group      = "${aws_iam_group.admins.name}"
-  policy_arn = "${data.aws_iam_policy.administrator_access.arn}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
