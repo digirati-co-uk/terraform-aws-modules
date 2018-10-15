873bed686f3f7e149ae478f0168fbcda4b23dd16
Adam Christie <fractos@gmail.com>
2018-10-15 17:08:47 +0100

    removing non-existent dependency.

--- tf/modules/services/base/worker/main.tf
+++ tf/modules/services/base/worker/main.tf
@@ -7,8 +7,4 @@ resource "aws_ecs_service" "service" {
   cluster         = "${var.cluster_id}"
   task_definition = "${var.task_definition_arn}"
   desired_count   = "${var.desired_count}"
-
-  depends_on = [
-    "aws_iam_role_policy.service",
-  ]
 }
