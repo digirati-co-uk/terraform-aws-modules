dcc8319ae32c0bc72dea6b7ce8cd1caf4e153f70
Adam Christie <fractos@gmail.com>
2018-10-22 11:16:53 +0100

    adding output of rendered task definition json.

--- tf/modules/services/tasks/base/output.tf
+++ tf/modules/services/tasks/base/output.tf
@@ -3,6 +3,11 @@ output "task_definition_arn" {
   value       = "${length(var.mount_points) == 0 ? aws_ecs_task_definition.task.arn : ""}"
 }
 
+output "task_definition_json" {
+  description = "JSON of the rendered task definition"
+  value       = "${data.template_file.definition.rendered}"
+}
+
 output "role_arn" {
   description = "ARN of the task role"
   value       = "${aws_iam_role.task.arn}"
