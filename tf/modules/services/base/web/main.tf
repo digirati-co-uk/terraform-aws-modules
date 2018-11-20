b1545ed7736e8f604997034fa594fd61c7758aa1
Adam Christie <fractos@gmail.com>
2018-11-20 11:46:01 +0000

    make creating a route53 entry optional for services.

--- tf/modules/services/base/web/main.tf
+++ tf/modules/services/base/web/main.tf
@@ -253,6 +253,7 @@ resource "aws_alb_listener_rule" "https" {
 #######
 
 resource "aws_route53_record" "service" {
+  count   = "${var.create_route53_entry == true ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "${var.hostname}.${var.domain}"
   type    = "A"
