6fbdd332294eafa7e6305c2440c5309824d55424
Adam Christie <fractos@gmail.com>
2019-06-05 23:39:46 +0100

    adding triad and quartet task modules.

---
+++ tf/modules/services/tasks/triad/main.tf
@@ -0,0 +1,117 @@
+locals {
+  command_main           = "${jsonencode(var.command_main)}"
+  mount_points_main      = "${jsonencode(var.mount_points_main)}"
+  command_sidecar_1      = "${jsonencode(var.command_sidecar_1)}"
+  command_sidecar_2      = "${jsonencode(var.command_sidecar_2)}"
+  mount_points_sidecar_1 = "${jsonencode(var.mount_points_sidecar_1)}"
+  mount_points_sidecar_2 = "${jsonencode(var.mount_points_sidecar_2)}"
+  links_main             = "${jsonencode(var.links_main)}"
+  links_sidecar_1        = "${jsonencode(var.links_sidecar_1)}"
+  links_sidecar_2        = "${jsonencode(var.links_sidecar_2)}"
+  port_mappings          = "${var.port_mappings_length > 0 ? module.port_mappings.port_mappings_string : "[{\"hostPort\": ${var.host_port}, \"containerPort\": ${var.container_port}, \"protocol\": \"tcp\"}]"}"
+  user_main              = "${length(var.user_main) > 0 ? "\"${var.user_main}\"" : "null"}"
+  user_sidecar_1         = "${length(var.user_sidecar_1) > 0 ? "\"${var.user_sidecar_1}\"" : "null"}"
+  user_sidecar_2         = "${length(var.user_sidecar_2) > 0 ? "\"${var.user_sidecar_2}\"" : "null"}"
+}
+
+data "template_file" "definition" {
+  template = "${file("${path.module}/files/task_definition.jsontemplate")}"
+
+  vars {
+    prefix                          = "${var.prefix}"
+    log_group_name                  = "${var.log_group_name}"
+    log_group_region                = "${var.log_group_region}"
+    log_prefix                      = "${var.log_prefix}"
+    environment_variables_main      = "${module.env_vars_main.env_vars_string}"
+    environment_variables_sidecar_1 = "${module.env_vars_sidecar_1.env_vars_string}"
+    environment_variables_sidecar_2 = "${module.env_vars_sidecar_2.env_vars_string}"
+    container_name_main             = "${var.container_name_main}"
+    container_name_sidecar_1        = "${var.container_name_sidecar_1}"
+    container_name_sidecar_2        = "${var.container_name_sidecar_2}"
+    container_port                  = "${var.container_port}"
+    docker_image_main               = "${var.docker_image_main}"
+    docker_image_sidecar_1          = "${var.docker_image_sidecar_1}"
+    docker_image_sidecar_2          = "${var.docker_image_sidecar_2}"
+    cpu_reservation_main            = "${var.cpu_reservation_main}"
+    cpu_reservation_sidecar_1       = "${var.cpu_reservation_sidecar_1}"
+    cpu_reservation_sidecar_2       = "${var.cpu_reservation_sidecar_2}"
+    memory_reservation_main         = "${var.memory_reservation_main}"
+    memory_reservation_sidecar_1    = "${var.memory_reservation_sidecar_1}"
+    memory_reservation_sidecar_2    = "${var.memory_reservation_sidecar_2}"
+    command_main                    = "${local.command_main}"
+    command_sidecar_1               = "${local.command_sidecar_1}"
+    command_sidecar_2               = "${local.command_sidecar_2}"
+    mount_points_main               = "${local.mount_points_main}"
+    mount_points_sidecar_1          = "${local.mount_points_sidecar_1}"
+    mount_points_sidecar_2          = "${local.mount_points_sidecar_2}"
+    links_main                      = "${local.links_main}"
+    links_sidecar_1                 = "${local.links_sidecar_1}"
+    links_sidecar_2                 = "${local.links_sidecar_2}"
+    user_main                       = "${local.user_main}"
+    user_sidecar_1                  = "${local.user_sidecar_1}"
+    user_sidecar_2                  = "${local.user_sidecar_2}"
+
+    ulimits_main      = "${module.ulimits_main.ulimits_string}"
+    ulimits_sidecar_1 = "${module.ulimits_sidecar_1.ulimits_string}"
+    ulimits_sidecar_2 = "${module.ulimits_sidecar_2.ulimits_string}"
+
+    port_mappings = "${local.port_mappings}"
+  }
+}
+
+# produce a task definition as long as there are no mount points - services that need mount points will have to define their own resource outside of the module
+resource "aws_ecs_task_definition" "task" {
+  count                 = "${length(var.mount_points_main) == 0 && length(var.mount_points_sidecar) == 0  ? 1 : 0}"
+  family                = "${var.family}"
+  container_definitions = "${data.template_file.definition.rendered}"
+  task_role_arn         = "${aws_iam_role.task.arn}"
+}
+
+module "env_vars_main" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/environment-variables/"
+
+  env_vars        = "${var.environment_variables_main}"
+  env_vars_length = "${var.environment_variables_length_main}"
+}
+
+module "env_vars_sidecar_1" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/environment-variables/"
+
+  env_vars        = "${var.environment_variables_sidecar_1}"
+  env_vars_length = "${var.environment_variables_length_sidecar_1}"
+}
+
+module "env_vars_sidecar_2" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/environment-variables/"
+
+  env_vars        = "${var.environment_variables_sidecar_2}"
+  env_vars_length = "${var.environment_variables_length_sidecar_2}"
+}
+
+module "ulimits_main" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/ulimits/"
+
+  ulimits        = "${var.ulimits_main}"
+  ulimits_length = "${var.ulimits_main_length}"
+}
+
+module "ulimits_sidecar_1" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/ulimits/"
+
+  ulimits        = "${var.ulimits_sidecar_1}"
+  ulimits_length = "${var.ulimits_sidecar_1_length}"
+}
+
+module "ulimits_sidecar_2" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/ulimits/"
+
+  ulimits        = "${var.ulimits_sidecar_2}"
+  ulimits_length = "${var.ulimits_sidecar_2_length}"
+}
+
+module "port_mappings" {
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/port-mappings/"
+
+  port_mappings        = "${var.port_mappings}"
+  port_mappings_length = "${var.port_mappings_length}"
+}
---
+++ tf/modules/services/tasks/triad/variables.tf
@@ -0,0 +1,232 @@
+variable "prefix" {
+  description = "Prefix for AWS resources"
+}
+
+variable "log_group_name" {
+  description = "Name of CloudWatch Logs group"
+}
+
+variable "log_group_region" {
+  description = "CloudWatch Logs group region"
+}
+
+variable "log_prefix" {
+  description = "Prefix for CloudWatch Logs within group"
+}
+
+variable "docker_image_main" {
+  description = "URL of Docker image to use for main"
+}
+
+variable "docker_image_sidecar_1" {
+  description = "URL of Docker image to use for sidecar 1"
+}
+
+variable "docker_image_sidecar_2" {
+  description = "URL of Docker image to use for sidecar 2"
+}
+
+variable "family" {
+  description = "Task Definition family name"
+}
+
+variable "container_port" {
+  description = "Port that the container is expecting to receive traffic on"
+  default     = ""
+}
+
+variable "container_name_main" {
+  description = "Name of the main container"
+  default     = ""
+}
+
+variable "container_name_sidecar_1" {
+  description = "Name of the sidecar 1 container"
+  default     = ""
+}
+
+variable "container_name_sidecar_2" {
+  description = "Name of the sidecar 2 container"
+  default     = ""
+}
+
+variable "host_port" {
+  description = "Optional - Port that the host will receive traffic on"
+  default     = "0"
+}
+
+variable "cpu_reservation_main" {
+  description = "Amount of CPU to reserve for task in main container"
+  default     = 0
+}
+
+variable "cpu_reservation_sidecar_1" {
+  description = "Amount of CPU to reserve for task in sidecar 1 container"
+  default     = 0
+}
+
+variable "cpu_reservation_sidecar_2" {
+  description = "Amount of CPU to reserve for task in sidecar 2 container"
+  default     = 0
+}
+
+variable "memory_reservation_main" {
+  description = "Amount of memory to reserve for task in main container"
+  default     = 128
+}
+
+variable "memory_reservation_sidecar_1" {
+  description = "Amount of memory to reserve for task in sidecar 1 container"
+  default     = 128
+}
+
+variable "memory_reservation_sidecar_2" {
+  description = "Amount of memory to reserve for task in sidecar 2 container"
+  default     = 128
+}
+
+variable "environment_variables_main" {
+  description = "Map of environment variables for main container"
+  type        = "map"
+  default     = {}
+}
+
+variable "environment_variables_sidecar_1" {
+  description = "Map of environment variables for sidecar 1 container"
+  type        = "map"
+  default     = {}
+}
+
+variable "environment_variables_sidecar_2" {
+  description = "Map of environment variables for sidecar 2 container"
+  type        = "map"
+  default     = {}
+}
+
+variable "environment_variables_length_main" {
+  description = "Length of environment variable map for main container (required)"
+  default     = 0
+}
+
+variable "environment_variables_length_sidecar_1" {
+  description = "Length of environment variable map for sidecar 1 container (required)"
+  default     = 0
+}
+
+variable "environment_variables_length_sidecar_2" {
+  description = "Length of environment variable map for sidecar 2 container (required)"
+  default     = 0
+}
+
+variable "command_main" {
+  description = "Override for main container command"
+  type        = "list"
+  default     = []
+}
+
+variable "command_sidecar_1" {
+  description = "Override for sidecar 1 container command"
+  type        = "list"
+  default     = []
+}
+
+variable "command_sidecar_2" {
+  description = "Override for sidecar 2 container command"
+  type        = "list"
+  default     = []
+}
+
+variable "mount_points_main" {
+  description = "List of main container mount points"
+  type        = "list"
+  default     = []
+}
+
+variable "mount_points_sidecar_1" {
+  description = "List of sidecar 1 container mount points"
+  type        = "list"
+  default     = []
+}
+
+variable "mount_points_sidecar_2" {
+  description = "List of sidecar 2 container mount points"
+  type        = "list"
+  default     = []
+}
+
+variable "links_main" {
+  description = "List of links for main container"
+  type        = "list"
+  default     = []
+}
+
+variable "links_sidecar_1" {
+  description = "List of links for sidecar 1 container"
+  type        = "list"
+  default     = []
+}
+
+variable "links_sidecar_2" {
+  description = "List of links for sidecar 2 container"
+  type        = "list"
+  default     = []
+}
+
+variable "ulimits_main" {
+  description = "Map of ulimits for main container"
+  type        = "map"
+  default     = {}
+}
+
+variable "ulimits_main_length" {
+  description = "Length of ulimits map for main container (required)"
+  default     = 0
+}
+
+variable "ulimits_sidecar_1" {
+  description = "Map of ulimits for sidecar 1 container"
+  type        = "map"
+  default     = {}
+}
+
+variable "ulimits_sidecar_2" {
+  description = "Map of ulimits for sidecar 2 container"
+  type        = "map"
+  default     = {}
+}
+
+variable "ulimits_sidecar_1_length" {
+  description = "Length of ulimits map for sidecar 1 container (required)"
+  default     = 0
+}
+
+variable "ulimits_sidecar_2_length" {
+  description = "Length of ulimits map for sidecar 2 container (required)"
+  default     = 0
+}
+
+variable "port_mappings" {
+  description = "Map of port_mappings"
+  type        = "map"
+  default     = {}
+}
+
+variable "port_mappings_length" {
+  description = "Length of port_mappings map (required)"
+  default     = 0
+}
+
+variable "user_main" {
+  description = "User to set for main container"
+  default     = ""
+}
+
+variable "user_sidecar_1" {
+  description = "User to set for sidecar 1 container"
+  default     = ""
+}
+
+variable "user_sidecar_2" {
+  description = "User to set for sidecar 2 container"
+  default     = ""
+}
