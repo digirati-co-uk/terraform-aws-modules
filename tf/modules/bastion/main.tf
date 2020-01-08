dc6630aabca503017ba7dfd665af91f49f4ab5e0
Adam Christie <fractos@gmail.com>
2020-01-08 15:13:17 +0000

    fixes

--- tf/modules/bastion/main.tf
+++ tf/modules/bastion/main.tf
@@ -35,7 +35,7 @@ data "aws_iam_policy_document" "assume_role_policy_ec2" {
       "sts:AssumeRole",
     ]
 
-    principals {
+    principals = {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
