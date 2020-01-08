87801b949883ae2b41bb80bfdf6bf9d0d2645de9
Adam Christie <fractos@gmail.com>
2020-01-08 15:24:58 +0000

    vpc changes for terraform 0.12

--- tf/modules/vpc/main.tf
+++ tf/modules/vpc/main.tf
@@ -1,170 +1,170 @@
 # VPC module
 
 resource "aws_vpc" "main" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-vpc"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_subnet" "public_1" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.subnet_public_1_cidr}"
-  availability_zone = "${var.subnet_public_1_az}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public_1_cidr
+  availability_zone = var.subnet_public_1_az
 
   map_public_ip_on_launch = true
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-public-1"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_subnet" "public_2" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.subnet_public_2_cidr}"
-  availability_zone = "${var.subnet_public_2_az}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public_2_cidr
+  availability_zone = var.subnet_public_2_az
 
   map_public_ip_on_launch = true
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-public-2"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_route_table" "public_1" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags {
+  tags = {
     "Name" = "${var.prefix}-public-1"
   }
 }
 
 resource "aws_route_table" "public_2" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags {
+  tags = {
     "Name" = "${var.prefix}-public-2"
   }
 }
 
 resource "aws_route_table_association" "public_1" {
-  subnet_id      = "${aws_subnet.public_1.id}"
-  route_table_id = "${aws_route_table.public_1.id}"
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_1.id
 }
 
 resource "aws_route_table_association" "public_2" {
-  subnet_id      = "${aws_subnet.public_2.id}"
-  route_table_id = "${aws_route_table.public_2.id}"
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_2.id
 }
 
 resource "aws_route" "public_1_internet_access" {
-  route_table_id         = "${aws_route_table.public_1.id}"
+  route_table_id         = aws_route_table.public_1.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.main.id}"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_route" "public_2_internet_access" {
-  route_table_id         = "${aws_route_table.public_2.id}"
+  route_table_id         = aws_route_table.public_2.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.main.id}"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "nat" {
   vpc = true
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-nat"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_nat_gateway" "gw" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.public_1.id}"
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-nat"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 
   depends_on = ["aws_internet_gateway.main"]
 }
 
 resource "aws_subnet" "private_1" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.subnet_private_1_cidr}"
-  availability_zone = "${var.subnet_private_1_az}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_private_1_cidr
+  availability_zone = var.subnet_private_1_az
 
   map_public_ip_on_launch = false
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-private-1"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_subnet" "private_2" {
-  vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.subnet_private_2_cidr}"
-  availability_zone = "${var.subnet_private_2_az}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_private_2_cidr
+  availability_zone = var.subnet_private_2_az
 
   map_public_ip_on_launch = false
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-private-2"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_route_table" "private_1" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-private-1"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_route_table" "private_2" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags {
+  tags = {
     "Name"    = "${var.prefix}-private-2"
-    "Project" = "${var.project}"
+    "Project" = var.project
   }
 }
 
 resource "aws_route_table_association" "private_1" {
-  subnet_id      = "${aws_subnet.private_1.id}"
-  route_table_id = "${aws_route_table.private_1.id}"
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
 }
 
 resource "aws_route_table_association" "private_2" {
-  subnet_id      = "${aws_subnet.private_2.id}"
-  route_table_id = "${aws_route_table.private_2.id}"
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_2.id
 }
 
 resource "aws_route" "private_1_internet_access" {
-  route_table_id         = "${aws_route_table.private_1.id}"
+  route_table_id         = aws_route_table.private_1.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.gw.id}"
+  nat_gateway_id         = aws_nat_gateway.gw.id
 
   depends_on = ["aws_nat_gateway.gw"]
 }
 
 resource "aws_route" "private_2_internet_access" {
-  route_table_id         = "${aws_route_table.private_2.id}"
+  route_table_id         = aws_route_table.private_2.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.gw.id}"
+  nat_gateway_id         = aws_nat_gateway.gw.id
 
   depends_on = ["aws_nat_gateway.gw"]
 }
