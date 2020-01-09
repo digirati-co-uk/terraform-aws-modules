cd51d1d04e1379a4ae17e4110967c5ffa56ef4e5
Adam Christie <fractos@gmail.com>
2020-01-09 12:07:49 +0000

    fixes

--- tf/modules/services/tasks/base/main.tf
+++ tf/modules/services/tasks/base/main.tf
@@ -1,9 +1,9 @@
 locals {
-  command       = "jsonencode(var.command)
-  mount_points  = "jsonencode(var.mount_points)
-  volumes_from  = "jsonencode(var.volumes_from)
-  port_mappings = "var.port_mappings_length > 0 ? module.port_mappings.port_mappings_string : "[{\"hostPort\": ${var.host_port}, \"containerPort\": ${var.container_port}, \"protocol\": \"tcp\"}]"
-  user          = "length(var.user) > 0 ? "\"${var.user}\"" : "null"
+  command       = jsonencode(var.command)
+  mount_points  = jsonencode(var.mount_points)
+  volumes_from  = jsonencode(var.volumes_from)
+  port_mappings = var.port_mappings_length > 0 ? module.port_mappings.port_mappings_string : "[{\"hostPort\": ${var.host_port}, \"containerPort\": ${var.container_port}, \"protocol\": \"tcp\"}]"
+  user          = length(var.user) > 0 ? "\"${var.user}\"" : "null"
 }
 
 data "template_file" "definition" {
@@ -32,29 +32,29 @@ data "template_file" "definition" {
 
 # produce a task definition as long as there are no mount points - services that need mount points will have to define their own resource outside of the module
 resource "aws_ecs_task_definition" "task" {
-  count                 = "length(var.mount_points) == 0 ? 1 : 0
-  family                = "var.family
-  container_definitions = "data.template_file.definition.rendered
-  task_role_arn         = "aws_iam_role.task.arn
+  count                 = length(var.mount_points) == 0 ? 1 : 0
+  family                = var.family
+  container_definitions = data.template_file.definition.rendered
+  task_role_arn         = aws_iam_role.task.arn
 }
 
 module "env_vars" {
   source = "../environment-variables/"
 
-  env_vars        = "var.environment_variables
-  env_vars_length = "var.environment_variables_length
+  env_vars        = var.environment_variables
+  env_vars_length = var.environment_variables_length
 }
 
 module "ulimits" {
   source = "../ulimits/"
 
-  ulimits        = "var.ulimits
-  ulimits_length = "var.ulimits_length
+  ulimits        = var.ulimits
+  ulimits_length = var.ulimits_length
 }
 
 module "port_mappings" {
   source = "../port-mappings/"
 
-  port_mappings        = "var.port_mappings
-  port_mappings_length = "var.port_mappings_length
+  port_mappings        = var.port_mappings
+  port_mappings_length = var.port_mappings_length
 }
