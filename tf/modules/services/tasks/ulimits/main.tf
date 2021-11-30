65f50d62ba33bcc0d56d763997c547afc5f882bc
Donald Gray <donald.gray@digirati.com>
2021-11-30 11:39:35 +0000

    Fix ulimits module to use var, not local

--- tf/modules/services/tasks/ulimits/main.tf
+++ tf/modules/services/tasks/ulimits/main.tf
@@ -1,12 +1,12 @@
 locals {
-  ulimits_keys = keys(local.ulimits)
+  ulimits_keys = keys(var.ulimits)
   sorted_ulimits_keys = sort(local.ulimits_keys)
 
   vars = [
     for k in local.sorted_ulimits_keys : {
       name = k
-      hardLimit = tonumber(split(":", lookup(local.ulimits, k))[0])
-      softLimit = tonumber(split(":", lookup(local.ulimits, k))[1])
+      hardLimit = tonumber(split(":", lookup(var.ulimits, k))[0])
+      softLimit = tonumber(split(":", lookup(var.ulimits, k))[1])
     }
   ]
 
