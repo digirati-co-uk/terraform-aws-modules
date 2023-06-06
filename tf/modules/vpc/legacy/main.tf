ef65a711277c8a94b8c3537f48c5ee89e55875fd
Donald Gray <donald.gray@digirati.com>
2023-06-06 14:36:36 +0100

    Add legacy vpc module

---
+++ tf/modules/vpc/legacy/main.tf
@@ -0,0 +1,161 @@
+# VPC module
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name"    = "${var.prefix}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public_1_cidr
+  availability_zone = var.subnet_public_1_az
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"    = "${var.prefix}-public-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public_2_cidr
+  availability_zone = var.subnet_public_2_az
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"    = "${var.prefix}-public-2"
+  }
+}
+
+resource "aws_route_table" "public_1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name" = "${var.prefix}-public-1"
+  }
+}
+
+resource "aws_route_table" "public_2" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name" = "${var.prefix}-public-2"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_1.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_2.id
+}
+
+resource "aws_route" "public_1_internet_access" {
+  route_table_id         = aws_route_table.public_1.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "public_2_internet_access" {
+  route_table_id         = aws_route_table.public_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    "Name"    = "${var.prefix}-nat"
+  }
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    "Name"    = "${var.prefix}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_private_1_cidr
+  availability_zone = var.subnet_private_1_az
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name"    = "${var.prefix}-private-1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_private_2_cidr
+  availability_zone = var.subnet_private_2_az
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name"    = "${var.prefix}-private-2"
+  }
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name"    = "${var.prefix}-private-1"
+  }
+}
+
+resource "aws_route_table" "private_2" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name"    = "${var.prefix}-private-2"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_2.id
+}
+
+resource "aws_route" "private_1_internet_access" {
+  route_table_id         = aws_route_table.private_1.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.gw.id
+
+  depends_on = [aws_nat_gateway.gw]
+}
+
+resource "aws_route" "private_2_internet_access" {
+  route_table_id         = aws_route_table.private_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.gw.id
+
+  depends_on = [aws_nat_gateway.gw]
+}
---
+++ tf/modules/vpc/legacy/output.tf
@@ -0,0 +1,55 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_public_1_id" {
+  value = aws_subnet.public_1.id
+}
+
+output "subnet_public_2_id" {
+  value = aws_subnet.public_2.id
+}
+
+output "subnet_private_1_id" {
+  value = aws_subnet.private_1.id
+}
+
+output "subnet_private_2_id" {
+  value = aws_subnet.private_2.id
+}
+
+output "route_table_public_1_id" {
+  value = aws_route_table.public_1.id
+}
+
+output "route_table_public_2_id" {
+  value = aws_route_table.public_2.id
+}
+
+output "route_table_private_1_id" {
+  value = aws_route_table.private_1.id
+}
+
+output "route_table_private_2_id" {
+  value = aws_route_table.private_2.id
+}
+
+output "subnet_public_1_az" {
+  value = var.subnet_public_1_az
+}
+
+output "subnet_public_2_az" {
+  value = var.subnet_public_2_az
+}
+
+output "subnet_private_1_az" {
+  value = var.subnet_private_1_az
+}
+
+output "subnet_private_2_az" {
+  value = var.subnet_private_2_az
+}
+
+output "nat_public_ip" {
+  value = aws_nat_gateway.gw.public_ip
+}
