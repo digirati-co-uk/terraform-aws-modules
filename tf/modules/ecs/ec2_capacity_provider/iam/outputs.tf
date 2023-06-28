dcce5c0c68e38b0b3c9039de91eee98a7f3bccbd
Donald Gray <donald.gray@digirati.com>
2023-06-28 10:49:42 +0100

    Output instance role name for capacity providers
    This is to allow adding more granular permissions to cluster

--- tf/modules/ecs/ec2_capacity_provider/iam/outputs.tf
+++ tf/modules/ecs/ec2_capacity_provider/iam/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_profile_arn" {
   value = aws_iam_instance_profile.instance_profile.arn
 }
+
+output "instance_role_name" {
+  value = aws_iam_role.instance_role.id
+}
--- tf/modules/ecs/ec2_capacity_provider_abs/outputs.tf
+++ tf/modules/ecs/ec2_capacity_provider_abs/outputs.tf
@@ -1,3 +1,7 @@
 output "name" {
   value = aws_ecs_capacity_provider.capacity_provider.name
 }
+
+output "instance_role_name" {
+  value = module.iam.instance_role_name
+}
